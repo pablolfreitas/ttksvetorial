c8dd1e35c8ad6709a1cd62b47b8caad661981f8e
Pablo <51050129+pablolfreitas@users.noreply.github.com>
2026-02-27 22:13:00 -0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-// ============================================================
-//  CONFIGURE AQUI suas credenciais do Supabase
-//  Acesse: https://supabase.com → seu projeto → Settings → API
-// ============================================================
-const SUPABASE_URL = "https://piqxryppehlxkrotjwvi.supabase.co";
-const SUPABASE_KEY = "sb_publishable_LDt8aY-LV4f6j_1yAL0gZQ_16BXXrhe";
